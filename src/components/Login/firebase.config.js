094256e75b500b0732a15f8bf438738e44e66cfd
Mahfuzislam1695 <mahfuzislam1695@gmail.com>
2022-01-11 11:35:03 +0600

    first commit

---
+++ src/components/Login/firebase.config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCgowAyORyoR5Fx-9wDG_9kQx2AGEUPN9g",
+    authDomain: "burj-al-arab-9325d.firebaseapp.com",
+    projectId: "burj-al-arab-9325d",
+    storageBucket: "burj-al-arab-9325d.appspot.com",
+    //line missing
+  };
+
+  export default firebaseConfig;
